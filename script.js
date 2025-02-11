0a88b0e6faf7c54e890c3395833189447c627c9a
Om Patel <ompatel9213@gmail.com>
2025-02-10 20:51:38 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function revealContent() {
+    const content = document.getElementById('content');
+    if (content.style.display === 'none' || content.style.display === '') {
+        content.style.display = 'block';
+    } else {
+        content.style.display = 'none';
+    }
+} 
